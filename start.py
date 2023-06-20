be2b545909bb7d6c6e3902285ee41c8b88b7d018
“eightfy” <l44454545@gmail.com>
2023-06-20 22:58:37 +0800

    none

--- start.py
+++ start.py
@@ -8,10 +8,10 @@ from message import MyHandler
 import datetime
 
 TEST_ROOM_IDS = [
-    22637920, #monaka
+    22603245, # 永雏塔菲
+    #22637920, #monaka
     #23490978,
     #1438243, 
-    #22603245,
     ]
 
 
